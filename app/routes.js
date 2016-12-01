05c8ce04d100d7f6f18c47518fe3207c03943d58
AndyB400 <andy.brad@gmail.com>
2016-12-01 09:03:56 +0000

    Added update, edit and delete

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,7 @@
 // create a new express router
-const express = require('express'),
-  router = express.Router(),
-  mainController = require('./controllers/main.controller'),
+const express      = require('express'),
+  router           = express.Router(),
+  mainController   = require('./controllers/main.controller'),
   eventsController = require('./controllers/events.controller');
 
 // export router
@@ -11,17 +11,22 @@ module.exports = router;
 // main routes
 router.get('/', mainController.showHome);
 
-// events routes
-router.get('/events', 		eventsController.showEvents);
+// event routes
+router.get('/events',       eventsController.showEvents);
 
 // seed events
-router.get('/events/seed', 	eventsController.seedEvents);
+router.get('/events/seed',  eventsController.seedEvents);
+
+// create events
+router.get('/events/create',  eventsController.showCreate);
+router.post('/events/create', eventsController.processCreate);
+
+// edit events
+router.get('/events/:slug/edit', eventsController.showEdit);
+router.post('/events/:slug',     eventsController.processEdit);
+
+// delete events
+router.get('/events/:slug/delete', eventsController.deleteEvent);
 
 // show a single event
 router.get('/events/:slug', eventsController.showSingle);
-
-
-// create events
-//edit events
-
-// delete events
--- server.js
+++ server.js
@@ -1,29 +1,48 @@
-// Load environment variables
+// load environment variables
 require('dotenv').config();
 
 // grab our dependencies
-const express = require('express'),
-  app = express(),
-  port = process.env.PORT || 8080,
+const express    = require('express'),
+  app            = express(),
+  port           = process.env.PORT || 8080,
   expressLayouts = require('express-ejs-layouts'),
-  mongoose = require('mongoose');
+  mongoose       = require('mongoose'),
+  bodyParser     = require('body-parser'),
+  session        = require('express-session'),
+  cookieParser   = require('cookie-parser'),
+  flash          = require('connect-flash'),
+  expressValidator = require('express-validator');
 
-app.disable('x-powered-by');
+// configure our application ===================
+// set sessions and cookie parser
+app.use(cookieParser());
+app.use(session({
+  secret: process.env.SECRET, 
+  cookie: { maxAge: 60000 },
+  resave: false,    // forces the session to be saved back to the store
+  saveUninitialized: false  // dont save unmodified
+}));
+app.use(flash());
 
-// configure our application ======================
 // tell express where to look for static assets
-app.use(express.static(__dirname + '\\public'));
+app.use(express.static(__dirname + '/public'));
 
 // set ejs as our templating engine
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
 
+// connect to our database
 mongoose.connect(process.env.DB_URI);
 
-// set the routes ===================================
+// use body parser to grab info from a form
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressValidator());
+
+// set the routes =============================
 app.use(require('./app/routes'));
 
-// start our server =================================
+// start our server ===========================
 app.listen(port, () => {
   console.log(`App listening on http://localhost:${port}`);
 });
+
